c6d41e545da46dff9cf20414259407940ca74c43
AishGaurav <37971199+AishGaurav@users.noreply.github.com>
2020-07-13 19:39:03 +0530

    Update home.component.ts
    Added CheckOut Button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../services/products.service';
 import { Subscription } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,23 +12,34 @@ import { Subscription } from 'rxjs';
 export class HomeComponent implements OnInit , OnDestroy {
   productPage = false;
   checkoutPage = false;
+  checkoutButton = false;
 
   activeSubs1 : Subscription;
   activeSubs2 : Subscription;
+  activeSubs3 : Subscription;
 
   constructor(
-    private productService: ProductService
+    private productService: ProductService,
+    private authService: AuthService,
+    private router: Router,
   ) 
   { }
 
   ngOnInit() {
-    this.activeSubs1  = this.productService.isProductPage.subscribe(res=>{this.productPage = res;})
-    this.activeSubs2  = this.productService.isCheckOutPage.subscribe(res=>{this.checkoutPage = res;})
+    this.activeSubs1  = this.productService.isProductPage.subscribe(res=>{this.productPage = res;});
+    this.activeSubs2  = this.productService.isCheckOutPage.subscribe(res=>{this.checkoutPage = res;});
+    this.activeSubs3  = this.productService.isCheckoutButton.subscribe(res=>{this.checkoutButton = res;})
+  }
+
+  checkOut(){
+    this.authService.checkOut = true;
+    this.router.navigate(['checkout']);
   }
 
   ngOnDestroy(){
     this.activeSubs1.unsubscribe();
     this.activeSubs2.unsubscribe();
+    this.activeSubs3.unsubscribe();
   }
 
 }
