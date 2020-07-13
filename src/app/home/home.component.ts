ca6d5299e3981cd3e53628daf25021f6cfeadcf5
AishGaurav <37971199+AishGaurav@users.noreply.github.com>
2020-07-13 19:22:02 +0530

    Update home.component.ts
    added unsubscribe() on ngOnDestroy()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../services/products.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit , OnDestroy {
   productPage = false;
   checkoutPage = false;
+
+  activeSubs1 : Subscription;
+  activeSubs2 : Subscription;
 
   constructor(
     private productService: ProductService
@@ -17,8 +20,13 @@ export class HomeComponent implements OnInit {
   { }
 
   ngOnInit() {
-    this.productService.isProductPage.subscribe(res=>{this.productPage = res;})
-    this.productService.isCheckOutPage.subscribe(res=>{this.checkoutPage = res;})
+    this.activeSubs1  = this.productService.isProductPage.subscribe(res=>{this.productPage = res;})
+    this.activeSubs2  = this.productService.isCheckOutPage.subscribe(res=>{this.checkoutPage = res;})
+  }
+
+  ngOnDestroy(){
+    this.activeSubs1.unsubscribe();
+    this.activeSubs2.unsubscribe();
   }
 
 }
